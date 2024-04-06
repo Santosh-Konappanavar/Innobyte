38254575cbe4f496c8e74377876e2b0d24c52c9f
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2024-04-06 14:59:17 +0530

    ADD:deploy

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 require("./db/conn");
 const router = require("./Routes/router");
-const PORT = 1080;
+const PORT = process.env.PORT || 1080;
 
 //middleware for data commig from the frontend will converted into json form
 app.use(express.json());
